8dc7ba5a5bc0b0027db3b31e466e41c78d88ea95
Jefferson Nascimento Souza <74577956+jefferson0710@users.noreply.github.com>
2023-11-10 13:38:50 -0300

    10/11/2023
    adicionando o primeiro modal.

--- src/app/pages/drogas/drogas.component.ts
+++ src/app/pages/drogas/drogas.component.ts
@@ -42,3 +42,24 @@ export class DrogasComponent {
 
 
 }
+document.addEventListener('DOMContentLoaded', function () {
+    const openModalBtn = document.getElementById('openModalBtn');
+    const closeModalBtn = document.getElementById('closeModalBtn');
+    const modal = document.getElementById('myModal');
+
+    if (openModalBtn && closeModalBtn && modal) {
+        openModalBtn.addEventListener('click', function () {
+            modal.style.display = 'block';
+        });
+
+        closeModalBtn.addEventListener('click', function () {
+            modal.style.display = 'none';
+        });
+
+        window.addEventListener('click', function (event) {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+    }
+});
