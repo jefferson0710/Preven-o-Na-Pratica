fe0a694064a44f433e2bf3c5fa1e95460bec02bc
Jefferson Nascimento Souza <74577956+jefferson0710@users.noreply.github.com>
2023-11-24 01:12:15 -0300

    24.11.2023
    Criando alerta para o email.

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { EmailService } from 'src/app/serve';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-formulario',
@@ -11,7 +13,7 @@ export class FormularioComponent {
   emailDestino: string = '';
   experienciaUsuario: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private Router:Router) {}
 
   enviarEmail() {
     const emailService = new EmailService(this.http);
@@ -19,13 +21,18 @@ export class FormularioComponent {
     const assunto = 'Obrigado por comparilha sua Experiencia';
     const conteudo = `<p>${this.experienciaUsuario} </p>`;
 
-
     emailService.enviarEmail(destinatario, assunto, conteudo).subscribe(
       (resposta) => {
         console.log('E-mail enviado com sucesso', resposta);
+        alert('Email enviado com Sucesso.!')
+        setTimeout(() => {
+        this.Router.navigate(['/']);
+
+        }, 1000);
       },
       (erro) => {
         console.error('Erro ao enviar e-mail', erro);
+        alert('Digite um email valido')
       }
     );
   }
