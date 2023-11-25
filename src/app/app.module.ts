29cc73a6109d40144714584d6618520467ef2739
Jefferson Nascimento Souza <74577956+jefferson0710@users.noreply.github.com>
2023-11-25 12:45:25 -0300

    atualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule, ToastrService } from 'ngx-toastr'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,9 +35,19 @@ import { EmailService } from './serve';
     ReactiveFormsModule,
     HttpClientModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      preventDuplicates: true,
+      progressBar: true, // Adiciona uma barra de progresso
+      progressAnimation: 'increasing', // Animação da barra de progresso
+      tapToDismiss: true, // Desabilita o fechamento do toast ao clicar
+      extendedTimeOut: 1000, // Tempo extra para fechar após passar o mouse
+      toastClass: 'custom-toast', // Adiciona uma classe personalizada para estilização
+    }),
+    BrowserAnimationsModule
   ],
-  providers: [EmailService],
+  providers: [EmailService,ToastrService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { EmailService } from 'src/app/serve';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class FormularioComponent {
   emailDestino: string = '';
   experienciaUsuario: string = '';
 
-  constructor(private http: HttpClient, private Router:Router) {}
+  constructor(private http: HttpClient, private Router:Router,private toastr: ToastrService,) {}
 
   enviarEmail() {
     const emailService = new EmailService(this.http);
@@ -24,7 +25,7 @@ export class FormularioComponent {
     emailService.enviarEmail(destinatario, assunto, conteudo).subscribe(
       (resposta) => {
         console.log('E-mail enviado com sucesso', resposta);
-        alert('Email enviado com Sucesso.!')
+       this.toastr.success('E-mail enviado com sucesso.')
         setTimeout(() => {
         this.Router.navigate(['/']);
 
@@ -32,7 +33,7 @@ export class FormularioComponent {
       },
       (erro) => {
         console.error('Erro ao enviar e-mail', erro);
-        alert('Digite um email valido')
+        this.toastr.error('Digite um e-mail valido.')
       }
     );
   }
